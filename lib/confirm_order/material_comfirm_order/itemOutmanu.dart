5621485be6bc0dca9effcc863005aca31279aeb1
Boss Phumipat SB <phumipat2543@gmail.com>
2023-02-02 18:52:12 +0700

    p soun set

--- lib/confirm_order/material_comfirm_order/itemOutmanu.dart
+++ lib/confirm_order/material_comfirm_order/itemOutmanu.dart
@@ -8,9 +8,12 @@ import 'package:test1/RAW/transform/transform.dart';
 import 'package:test1/material_new_order/compronan.dart';
 
 import '../../models/provider_app.dart';
+
 class itemOutmanu extends StatefulWidget {
-  itemOutmanu({Key? key,}) : super(key: key);
-  
+  itemOutmanu({
+    Key? key,
+  }) : super(key: key);
+
   @override
   State<itemOutmanu> createState() => _itemOutmanuState();
 }
@@ -20,50 +23,55 @@ class _itemOutmanuState extends State<itemOutmanu> {
   @override
   Widget build(BuildContext context) {
     var tasks = context.watch<provider_app>().tasks;
-    
-  
-    return Container(
-      child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-        return tasks.length <= 0
-            ? Center(
-                child: Text(
-                  '''NO ITEM''',
-                  overflow: TextOverflow.visible,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    height: 1.2102272327129657,
-                    fontSize: 20,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w400,
-                    color: Color.fromARGB(255, 255, 110, 110),
-                    decoration: TextDecoration.underline,
-                    /* letterSpacing: 0.0, */
-                  ),
-                ),
-              )
-            : ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, int index) {
-                  return Expanded(
-                     
-                     child: Row(
-                      children: [
-                        Container(
-                          child:SizedBox(
-                            height: double.maxFinite,
-                            child: Text("data"),
-                          ),
-                        )
-                      ],
 
-                     ),
-                        
-
+    return Flexible(
+        flex: 1,
+        fit: FlexFit.tight,
+        child: Container(
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            return tasks.length <= 0
+                ? Center(
+                    child: Text(
+                      '''NO ITEM''',
+                      overflow: TextOverflow.visible,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        height: 1.2102272327129657,
+                        fontSize: 20,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        color: Color.fromARGB(255, 255, 110, 110),
+                        decoration: TextDecoration.underline,
+                        /* letterSpacing: 0.0, */
+                      ),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: tasks.length,
+                    itemBuilder: (context, int index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Colors.white),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              fit: FlexFit.tight,
+                              child: Text('1234'),
+                            ),
+                             Flexible(
+                              flex: 1,
+                              fit: FlexFit.tight,
+                              child: Text('1234'),
+                            )
+                          ],
+                        ),
                       );
-                },
-              );
-      }),
-    );
+                    },
+                  );
+          }),
+        ));
   }
 }
